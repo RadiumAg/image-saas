bb5d704fedc5343950b17a7ad6d151c80d947458
Radium <2043869032@qq.com>
2026-01-02 16:33:05 +0800

    refactor: remove unused TagService import

--- src/app/api/files/[fileId]/tags/route.ts
+++ src/app/api/files/[fileId]/tags/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
-import { TagService } from '@/server/services/tag-service';
 
 export async function GET(
   request: NextRequest,
