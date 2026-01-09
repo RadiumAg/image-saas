48b624332c1c5ec2f92d029bf1baf2797160b0be
Radium <2043869032@qq.com>
2026-01-09 22:16:19 +0800

    refactor: switch from neon to postgres.js database client

--- src/server/db/db.ts
+++ src/server/db/db.ts
@@ -1,14 +1,8 @@
-// Make sure to install the 'postgres' package
-// import { drizzle } from 'drizzle-orm/postgres-js';
-// import postgres from 'postgres';
 import * as schema from './schema';
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
 
-// const queryClient = postgres(process.env.DATABASE_URL!);
-// const db = drizzle(queryClient, { schema });
-
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle({ client: sql, schema });
+const queryClient = postgres(process.env.DATABASE_URL!);
+const db = drizzle(queryClient, { schema });
 
 export { db };
