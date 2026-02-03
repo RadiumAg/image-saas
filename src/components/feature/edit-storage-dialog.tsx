be57673fe1ebc4a1483305eba33e2c1d7f31c4b7
RadiumAg <2043869032@qq.com>
2026-02-04 01:10:01 +0800

    refactor: remove unused React import in edit-storage-dialog

--- src/components/feature/edit-storage-dialog.tsx
+++ src/components/feature/edit-storage-dialog.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
